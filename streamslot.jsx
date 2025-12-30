30dd52a5837608e4e9e232babf8e008bf895612f
luis-ferreras <inbox.luisferreras@gmail.com>
2025-12-30 10:39:38 -0500

    Update streamslot.jsx

--- streamslot.jsx
+++ streamslot.jsx
@@ -3234,19 +3234,7 @@ select option {
 
           
               {/* AdSense code goes here */}
-              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1453329637761114" crossorigin="anonymous"></script>
-                <ins 
-                  class="adsbygoogle"
-                  style="display:block"
-                  data-ad-client="ca-pub-1453329637761114"
-                  data-ad-slot="4926680502"
-                  data-ad-format="auto"
-                  data-full-width-responsive="true">
-                </ins>
-                <script>
-                  (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-
+           
           
             
           
